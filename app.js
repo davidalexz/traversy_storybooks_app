dd33351c4241f3fc1a62d1f2f52cc3293269e300
davidalexz <alexz.chimi@gmail.com>
2023-01-28 19:30:17 +0200

    server setup

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const hbs = require('hbs')
 const connectDB = require('./config/db')
 
 // Load config 
@@ -11,10 +14,23 @@ connectDB()
 
  const app = express()
 
+// Logging
  if(process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'))
  }
 
+ // Handlebars
+
+app.engine('.hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', './views');
+
+// Static folder
+
+app.use(express.static('public'))
+
+// Routes
+app.use('/', require('./routes/index'))
 
 const PORT = process.env.PORT || 5000//we use process.env to access variable that are in our config file
 
